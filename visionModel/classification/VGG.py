8ef02b789ff62058680cf98bafe934cd7a966665
ldy <44680953+dongyulin89@users.noreply.github.com>
2022-03-02 17:53:02 +0800

    Create VGG.py

---
+++ visionModel/classification/VGG.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+
+class VGG(nn.Module):
+    
+    def __init__(
+        self,
+        features: nn.Module,
+        num_classes: int = 1000,
+        init_weights: bool = True
+    ) -> None:
+        super(VGG, self).__init__()
+        
+        self.features_vgg11 = nn.Sequential(
+            nn.conv2d(3, 64, kernel_size=3, padding=1), 
+            # nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.conv2d(64, 128, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.conv2d(128, 256, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.conv2d(256, 256, kernel_size=3, padding=1),
+            # nn.BarchNorm2d(256),
+            nn.ReLU(inpalce=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.conv2d(256, 512, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.conv2d(512, 512, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(512), 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.conv2d(512, 512, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.conv2d(512, 512, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(512), 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096), 
+            nn.ReLU(inplace=True), 
+            nn.Dropout(), 
+            
+            nn.Linear(4096, 4096), 
+            nn.ReLU(inplace=True), 
+            nn.Dropout(),
+            
+            nn.Linear(4096, num_classes), 
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features_vgg11(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        
+        return x
